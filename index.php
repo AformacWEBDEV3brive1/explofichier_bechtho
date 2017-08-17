21c4da22208f6b3f071d791fa73d766e7e26cd11
rackmaninov <rackmaninov@racmaninov>
2017-08-17 11:58:24 +0200

    first commit ajout librairies + page PHP + JS  +CSS

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+           <!-- libaries css-->
+            <link type="text/css" rel="stylesheet" href="libraries/bootstrap4/css/bootstrap.css" />
+
+        <!-- libraries js -->        
+            <script type="text/javascript" src="libraries/jQuery/jquery-3.2.1.js"></script>
+            <script type="text/javascript" src="libraries/tether/dist/js/tether.js"></script>
+            <script type="text/javascript" src="libraries/bootstrap-4.0.0-alpha.6-dist/js/bootstrap.js"></script>
+            
+        <!-- custom css  -->
+            <link type="text/css" rel="stylesheet" href="css/style.css" />
+        
+        <!-- custom js  -->
+            <script type="text/javascript" src="js/carrousel/main.js"></script>
+           <script src="jquery-3.2.1.min.js"></script>
+    </head>
+    <body>
+        <div class="entete">
+        </div>
+        <div class="contenu">
+            
+        </div>
+    </body>
+</html>
