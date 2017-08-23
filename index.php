10e6dce1968d6e1dace46e5ea1b4a5ab65eede3d
christian <christian@christian-HP-ProBook-450-G3>
2017-08-23 11:35:54 +0200

    index suppression d'une ligne link js javascript ajout d'une fonction pour rentrer son nom 			ajout d'un retour bloqué

--- index.php
+++ index.php
@@ -1,78 +1,60 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <meta charset="UTF-8">
+<head>
+<meta charset="UTF-8">
 
-        <title> Explorateur de fichier</title>
-        <!-- libaries css-->
-        <link type="text/css" rel="stylesheet" href="libraries/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css" />
-		<link rel="stylesheet" href="libraries/font-awesome-4.7.0/css/font-awesome.css"/>
-                
-           <!-- libaries css-->
-            <link type="text/css" rel="stylesheet" href="libraries/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css" />
+<title>Explorateur de fichier</title>
+<!-- libaries css-->
+<link type="text/css" rel="stylesheet" href="libraries/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css" />
+<link rel="stylesheet" href="libraries/font-awesome-4.7.0/css/font-awesome.css" />
 
-
-        <!-- libraries js -->        
-        <script type="text/javascript" src="libraries/jQuery/jquery-3.2.1.js"></script>
-        <script type="text/javascript" src="libraries/tether/dist/js/tether.js"></script>
-        <script type="text/javascript" src="libraries/bootstrap-4.0.0-alpha.6-dist/js/bootstrap.js"></script>
-        <script type="text/javascript" src="javascript.js"></script>
-
-        <!-- custom css  -->
-
-        <link type="text/css" rel="stylesheet" href="style.css" />
-
-        <!-- custom js  -->
+<!-- libaries css-->
+<link type="text/css" rel="stylesheet"
+	href="libraries/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css" />
 
 
+<!-- libraries js -->
+<script type="text/javascript" src="libraries/jQuery/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="libraries/tether/dist/js/tether.js"></script>
+<script type="text/javascript" src="libraries/bootstrap-4.0.0-alpha.6-dist/js/bootstrap.js"></script>
+<!-- <script type="text/javascript" src="javascript.js"></script>-->
 
-    </head>
-    <body>
-        
-        <!-- navbar -->
-        <div class="entete">
-            <div class="container navbar1">
-                <div class="row">
+<!-- custom css  -->
 
-                    <button type="submit" id="retour" onclick="retour()" class="btn btn-secondary col-2"> <-- </button>
-                    <button type="submit" class="btn btn-secondary col-1"> --> </button>
-                    <button type="submit" class="btn btn-secondary col-1"> / </button>              
-                    <input class="form-control col-5" type="text" placeholder="Rechercher">
-                    <button class="btn btn-outline-default col-1" type="submit">OK</button>
+<link type="text/css" rel="stylesheet" href="style.css" />
 
-                </div>
+<!-- custom js  -->
 
-                <div class="row">
 
-                    <input class="form-control  col-3" id="path" type="text" placeholder="/home/christian">
 
-                </div>  
-                
-            </div>
-            <div class="container navbar2">
-                <div class="row">
-                    <button type="button" onclick="retour()" id="retour" class="btn btn-secondary col-2"> <-- </button>
-                    <button type="button" class="btn btn-secondary col-2"> --> </button>
-                    <button type="submit" class="btn btn-secondary col-1"> / </button>
-                    <input class="form-control  col-3" id="path" type="text" placeholder="/home/christian">
-                    <input class="form-control col-3" type="text" placeholder="Rechercher">
-                    <button class="btn btn-outline-default col-1" type="submit">OK</button>                 
-                </div>       
-            </div>
-        </div>
+</head>
+<body>
 
-        
-        <!-- Dossiers/Fichiers -->
+	<!-- navbar -->
+	<div class="entete">
+		<div class="container">
+			<div class="row">
 
-        <div class="container contenu padding">
-            <p class="fichier"></p>
-           
-            
-         </div>   
-            
-            
-            <script type="text/javascript" src="javascript.js"></script>
-        
-    </body>
+				<button type="submit" id="retour" onclick="retour()"
+					class="btn btn-secondary col-4">Retour</button>
+
+				<input class="form-control  col-8" id="path" type="text"
+					placeholder="/home">
+			</div>
+		</div>
+
+	</div>
+
+
+	<!-- Dossiers/Fichiers -->
+
+	<div class="container contenu padding">
+		<p class="fichier"></p>
+
+
+	</div>
+<script type="text/javascript" src="javascript.js"></script>
+
+</body>
 </html>
